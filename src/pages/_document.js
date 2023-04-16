aac0f717ab297d266c5a8bf3a031c1cdd02e0e36
Tomas Vasquez <tomasdetloging@gmail.com>
2023-04-16 18:37:36 -0400

    add img files

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
+
           {/* <!-- Google Web Fonts --> */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
@@ -39,9 +40,14 @@ class MyDocument extends Document {
             integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
             crossOrigin="anonymous"
           ></link>
+
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+            integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
-          dsdfsd
           <div className="Container-xxl bg-white p-0">
             <Nav />
             <Main />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
-      <Head></Head>
       <h1>Hello</h1>
     </div>
   );
